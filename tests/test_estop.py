eaeaf751ee9ea9c55632129d6003267123ee2e3a
mmingyeong <mingyeong@khu.ac.kr>
2022-01-03 19:36:35 +0900

    estop has new commands and unit test

--- tests/test_estop.py
+++ tests/test_estop.py
@@ -8,24 +8,38 @@ from lvmecp.actor.actor import LvmecpActor as EcpActor
 
 
 @pytest.mark.asyncio
-async def test_estop(actor: EcpActor):
+async def test_estop_status(actor: EcpActor):
 
-    command = await actor.invoke_mock_command("estop")
+    command = await actor.invoke_mock_command("estop status")
     await command
 
     assert command.status.did_succeed
     assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["emergency"]["E_stop"] == 0
     assert command.replies[-2].message["status"]["emergency"]["E_status"] == 0
-    assert command.replies[-2].message["status"]["emergency"]["E_relay"] == 0
 
+@pytest.mark.asyncio
+async def test_estop_trigger(actor: EcpActor):
+
+    command = await actor.invoke_mock_command("estop trigger")
+    await command
+
+    assert command.status.did_succeed
+    assert len(command.replies) == 4
+    assert command.replies[-2].message["status"]["emergency"]["E_status"] == 1
+
+    command = await actor.invoke_mock_command("estop stop")
+    await command
+
+    assert command.status.did_succeed
+    assert len(command.replies) == 4
+    assert command.replies[-2].message["status"]["emergency"]["E_status"] == 0
 
 @pytest.mark.asyncio
 async def test_estop_fail_connect(actor: EcpActor):
 
     await actor.plcs[0].stop()
 
-    command = await actor.invoke_mock_command("estop")
+    command = await actor.invoke_mock_command("estop status")
     await command
 
     assert command.status.did_fail
