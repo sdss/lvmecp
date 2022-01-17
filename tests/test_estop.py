667f549f4ff707fcc4e9b903fb82fdd6a53f7bfd
mmingyeong <mingyeong@khu.ac.kr>
2022-01-17 15:16:36 +0900

    update unit test for telemetry, estop

--- tests/test_estop.py
+++ tests/test_estop.py
@@ -8,38 +8,22 @@ from lvmecp.actor.actor import LvmecpActor as EcpActor
 
 
 @pytest.mark.asyncio
-async def test_estop_status(actor: EcpActor):
+async def test_estop(actor: EcpActor):
 
-    command = await actor.invoke_mock_command("estop status")
-    await command
-
-    assert command.status.did_succeed
-    assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["emergency"]["E_status"] == 0
-
-@pytest.mark.asyncio
-async def test_estop_trigger(actor: EcpActor):
-
-    command = await actor.invoke_mock_command("estop trigger")
+    command = await actor.invoke_mock_command("estop")
     await command
 
     assert command.status.did_succeed
     assert len(command.replies) == 4
     assert command.replies[-2].message["status"]["emergency"]["E_status"] == 1
 
-    command = await actor.invoke_mock_command("estop stop")
-    await command
-
-    assert command.status.did_succeed
-    assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["emergency"]["E_status"] == 0
 
 @pytest.mark.asyncio
 async def test_estop_fail_connect(actor: EcpActor):
 
     await actor.plcs[0].stop()
 
-    command = await actor.invoke_mock_command("estop status")
+    command = await actor.invoke_mock_command("estop")
     await command
 
     assert command.status.did_fail
