f214cac0c5dbdf46e62195d5ce694d90521e1df8
mmingyeong <mingyeong@khu.ac.kr>
2022-01-27 14:31:13 +0900

    unit test -ing...

--- tests/test_dome.py
+++ tests/test_dome.py
@@ -29,15 +29,15 @@ async def test_dome_status(actor: EcpActor):
 
 
 @pytest.mark.asyncio
-async def test_dome_move(actor: EcpActor):
+async def test_dome_enable(actor: EcpActor):
 
-    command = await actor.invoke_mock_command("dome move")
+    command = await actor.invoke_mock_command("dome enable")
     await command
     assert command.status.did_succeed
     assert len(command.replies) == 4
     assert command.replies[-2].message["status"]["Dome"] == "OPEN"
 
-    command = await actor.invoke_mock_command("dome move")
+    command = await actor.invoke_mock_command("dome enable")
     await command
     assert command.status.did_fail
     # assert len(command.replies) == 4
