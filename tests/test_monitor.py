32975ada50109209550ef4dec1946c050157c79f
mmingyeong <mingyeong@khu.ac.kr>
2022-02-03 17:16:40 +0900

    mock line for LvmecpError

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -5,6 +5,7 @@
 import pytest
 
 from lvmecp.actor.actor import LvmecpActor as EcpActor
+from lvmecp.exceptions import LvmecpError
 
 
 @pytest.mark.asyncio
@@ -34,6 +35,17 @@ async def test_monitor_fail_connect(actor: EcpActor):
 async def test_monitor_bad_name(actor: EcpActor):
 
     command = await actor.invoke_mock_command("monitor ar")
+    await command
+
+    assert command.status.did_fail
+
+@pytest.mark.asyncio
+async def test_monitor_fails(actor: EcpActor, mocker):
+
+    #mocker.patch.object(actor.plcs[0], "send_command", return_value=None)
+    mocker.patch.object(actor.plcs[0], "send_command", side_effect=LvmecpError)
+
+    command = await actor.invoke_mock_command("monitor")
     await command
 
     assert command.status.did_fail
