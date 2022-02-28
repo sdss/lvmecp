58d01947ebe30c534b90b6aa2b76fa306148cf16
mmingyeong <mingyeong@khu.ac.kr>
2022-02-28 15:24:43 +0900

    pytest testing ...

--- tests/actor/test_command_dome.py
+++ tests/actor/test_command_dome.py
@@ -22,31 +22,3 @@ async def test_dome_status(actor: EcpActor):
     assert command.replies[-2].message["status"]["Dome"] == "CLOSE"
 
 
-@pytest.mark.asyncio
-async def test_dome_enable(actor: EcpActor):
-
-    command = await actor.invoke_mock_command("dome enable")
-    await command
-    assert command.status.did_succeed
-    assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["Dome"] == "OPEN"
-
-    command = await actor.invoke_mock_command("dome status")
-    await command
-
-    assert command.status.did_fail
-
-    time.sleep(10)
-
-    command = await actor.invoke_mock_command("dome status")
-    await command
-
-    assert command.status.did_succeed
-    assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["Dome"] == "OPEN"
-
-    command = await actor.invoke_mock_command("dome enable")
-    await command
-    assert command.status.did_succeed
-    assert len(command.replies) == 4
-    assert command.replies[-2].message["status"]["Dome"] == "CLOSE"
