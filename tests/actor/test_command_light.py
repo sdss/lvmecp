8eb244c57f5f0443e8078e915818453577d99e3d
mmingyeong <mingyeong@khu.ac.kr>
2022-03-04 17:52:23 +0900

    <TEST>:Delete unused lines in light test

--- tests/actor/test_command_light.py
+++ tests/actor/test_command_light.py
@@ -13,9 +13,6 @@ from lvmecp.exceptions import LvmecpError
 @pytest.mark.asyncio
 async def test_light_status(actor: EcpActor):
 
-    # assert controllers[0].name == 'dome'
-    # assert controllers[0].result == 1
-
     # status check of light
     command = await actor.invoke_mock_command("light status")
     await command
@@ -29,9 +26,6 @@ async def test_light_status(actor: EcpActor):
     assert command.replies[-2].message["status"]["UMA lights"] == 0
     assert command.replies[-2].message["status"]["Telescope room - bright"] == 0
     assert command.replies[-2].message["status"]["Telescope room - red"] == 0
-
-    # assert controllers[0].modules[1].name == "lights"
-    # assert controllers[0].result == 0
 
     # status check of light
     command = await actor.invoke_mock_command("light status cr")
